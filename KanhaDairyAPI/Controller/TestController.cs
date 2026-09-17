cbc0d4ae87bdac1c259651211db2b059ccaf07b0
Shubham Hupta <shubham.gupta19701@gmail.com>
2026-09-17 15:58:05 +0530

    add new controller

---
+++ KanhaDairyAPI/Controller/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KanhaDairyAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}
